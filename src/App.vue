da75e4a3abc6cec656ab173567890c02395adc73
bonvi83 <bonvi83@gmail.com>
2024-02-10 14:38:39 +0100

    casinooooooooooooo

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+
+// dal mitico Cappa
+// export default{
+//     data(){
+//         return{
+//             logo: '',
+//             images: [
+//                 {
+//                     image: '',
+//                     imageHover: ''
+//                 },
+//                 {
+//                     image: '',
+//                     imageHover: ''
+//                 },
+//                 {
+//                     image: '',
+//                     imageHover: ''
+//                 },
+//                 {
+//                     image: '',
+//                     imageHover: ''
+//                 },
+//                 {
+//                     image: '',
+//                     imageHover: ''
+//                 },
+//                 {
+//                     image: '',
+//                     imageHover: ''
+//                 },
+//             ],
+//         }
+//     },
+
+//     methods: {
+//     }
+// }
+
+</script>
+
+
+<template></template>
+
+
+<script></script>
